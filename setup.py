922b4ce0994677db737c868bcb82a1556a3e875c
João Paulo Cautter <jcautter@gmail.com>
2023-11-16 00:20:22 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='teste'
+    , version='0.0.1'
+    , description="Python module teste"
+    , author='João Paulo de Brito e Cautter'
+    , author_email='jcautter@gmail.com'
+#     , url='https://github.com/nvbn/import_from_github_com'
+    , license='MIT'
+#     , packages=find_packages(
+#         exclude=[
+#             'ez_setup'
+#             , 'examples'
+#             , 'tests'
+#             , 'release'
+#         ]
+#     )
+#     , include_package_data=True
+#     , zip_safe=False
+)
